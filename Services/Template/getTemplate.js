ee0bb9358889cc6d309c3b56f0ba5abbf7e16064
DBVCA <87826936+DBVCA@users.noreply.github.com>
2021-08-05 10:52:33 -0700

    Update getTemplate.js

--- Services/Template/getTemplate.js
+++ Services/Template/getTemplate.js
@@ -9,37 +9,37 @@ module.exports = function getTemplate(context, token, jsonTemplate,
     displayName, description, owner) {
 
     context.log('Getting template ' + jsonTemplate);
-
-    return new Promise((resolve, reject) => {
-
-        var template;   // The template as a JavaScript object
-
-		
-		context.log(settings().TENANT_NAME + ' ' + settings().TEMPLATE_SITE_URL + ' ' + settings().TEMPLATE_LIB_NAME);
-        // 1. Get ID of the SharePoint site where template files are stored
-        getSiteId(context, token, settings().TENANT_NAME, 
-            settings().TEMPLATE_SITE_URL)
-        .then((siteId) => {
-        // 2. Get the Graph API drive ID for the doc library where template files are stored
-             return getDriveId(context, token, siteId, settings().TEMPLATE_LIB_NAME);
-        })
-        .then((driveId) => {
-        // 3. Get the download URL for the template file
-            return getDownloadUrl(context, token, driveId,
-                `${jsonTemplate}${settings().TEMPLATE_FILE_EXTENSION}`);
-        })
-        .then((downloadUrl) => {
-        // 4. Get the contents of the template file
-            return downloadDriveItem(context, token, downloadUrl);
-        })
-        .then((templateString) => {
-
-        // 5. Parse the template; get owner's user ID
-            template = JSON.parse(templateString.trimLeft());
-            return getUserId (context, token, owner);
-        })
 	if( jsonTemplate == "GreenTemplate")
 	{
+		
+	    return new Promise((resolve, reject) => {
+
+		var template;   // The template as a JavaScript object
+	
+		context.log(settings().TENANT_NAME + ' ' + settings().TEMPLATE_SITE_URL + ' ' + settings().TEMPLATE_LIB_NAME);
+		// 1. Get ID of the SharePoint site where template files are stored
+		getSiteId(context, token, settings().TENANT_NAME, 
+		    settings().TEMPLATE_SITE_URL)
+		.then((siteId) => {
+		// 2. Get the Graph API drive ID for the doc library where template files are stored
+		     return getDriveId(context, token, siteId, settings().TEMPLATE_LIB_NAME);
+		})
+		.then((driveId) => {
+		// 3. Get the download URL for the template file
+		    return getDownloadUrl(context, token, driveId,
+			`${jsonTemplate}${settings().TEMPLATE_FILE_EXTENSION}`);
+		})
+		.then((downloadUrl) => {
+		// 4. Get the contents of the template file
+		    return downloadDriveItem(context, token, downloadUrl);
+		})
+		.then((templateString) => {
+
+		// 5. Parse the template; get owner's user ID
+		    template = JSON.parse(templateString.trimLeft());
+		    return getUserId (context, token, owner);
+		})
+	
 		
 		.then((ownerId) => {
 		// 6. Add the per-team properties to the template
@@ -55,13 +55,46 @@ module.exports = function getTemplate(context, token, jsonTemplate,
 				    'isFavoriteByDefault': true,
 				    'description': ''
 				}
-			    ]
+			    ];
 		// 7. Return the finished template as a string
 		    resolve(JSON.stringify(template));
 		})
+		.catch((ex) => {
+		    reject(`Error in getTemplate(): ${ex}`);
+		});
+	});	
 	}
 	else
 	{
+		return new Promise((resolve, reject) => {
+
+		var template;   // The template as a JavaScript object
+	
+		context.log(settings().TENANT_NAME + ' ' + settings().TEMPLATE_SITE_URL + ' ' + settings().TEMPLATE_LIB_NAME);
+		// 1. Get ID of the SharePoint site where template files are stored
+		getSiteId(context, token, settings().TENANT_NAME, 
+		    settings().TEMPLATE_SITE_URL)
+		.then((siteId) => {
+		// 2. Get the Graph API drive ID for the doc library where template files are stored
+		     return getDriveId(context, token, siteId, settings().TEMPLATE_LIB_NAME);
+		})
+		.then((driveId) => {
+		// 3. Get the download URL for the template file
+		    return getDownloadUrl(context, token, driveId,
+			`${jsonTemplate}${settings().TEMPLATE_FILE_EXTENSION}`);
+		})
+		.then((downloadUrl) => {
+		// 4. Get the contents of the template file
+		    return downloadDriveItem(context, token, downloadUrl);
+		})
+		.then((templateString) => {
+
+		// 5. Parse the template; get owner's user ID
+		    template = JSON.parse(templateString.trimLeft());
+		    return getUserId (context, token, owner);
+		})
+	
+		
 		.then((ownerId) => {
 		// 6. Add the per-team properties to the template
 
@@ -70,15 +103,15 @@ module.exports = function getTemplate(context, token, jsonTemplate,
 		    template['owners@odata.bind'] = [
 			`https://graph.microsoft.com/beta/users('${ownerId}')`
 		    ];
-
 		// 7. Return the finished template as a string
 		    resolve(JSON.stringify(template));
-		})	
+		})
+		.catch((ex) => {
+		    reject(`Error in getTemplate(): ${ex}`);
+		});
 	}
-        .catch((ex) => {
-            reject(`Error in getTemplate(): ${ex}`);
-        });
+        
 
 
-    });
+    
 }
